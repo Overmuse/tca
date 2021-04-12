f9a0c72c6216630c580add0c4e019f526128284c
Sebastian Rollen <seb@overmu.se>
2021-04-12 13:45:14 -0400

    more metrics

--- src/metrics/price_improvement.rs
+++ src/metrics/price_improvement.rs
@@ -6,7 +6,9 @@ use serde::Serialize;
 #[derive(Serialize, Debug)]
 pub struct PriceImprovement {
     pub amount: f64,
-    pub percentage: f64,
+    pub effective_spread: f64,
+    pub quoted_spread: f64,
+    pub efq: f64,
 }
 
 pub fn price_improvement(quote: &Quote, side: &Side, fill_price: f64) -> Result<PriceImprovement> {
@@ -20,15 +22,27 @@ pub fn price_improvement(quote: &Quote, side: &Side, fill_price: f64) -> Result<
         .as_ref()
         .ok_or_else(|| anyhow!("Missing ask quote"))?
         .price;
+    let mid_price = (bid_price + ask_price) / 2.0;
+    let quoted_spread = ask_price - bid_price;
     let res = match side {
-        Side::Buy => PriceImprovement {
-            amount: ask_price - fill_price,
-            percentage: 1.0 - fill_price / ask_price,
-        },
-        Side::Sell => PriceImprovement {
-            amount: fill_price - bid_price,
-            percentage: fill_price / bid_price - 1.0,
-        },
+        Side::Buy => {
+            let effective_spread = 2.0 * (fill_price - mid_price);
+            PriceImprovement {
+                amount: ask_price - fill_price,
+                effective_spread,
+                quoted_spread,
+                efq: effective_spread / quoted_spread,
+            }
+        }
+        Side::Sell => {
+            let effective_spread = 2.0 * (mid_price - fill_price);
+            PriceImprovement {
+                amount: fill_price - bid_price,
+                effective_spread,
+                quoted_spread,
+                efq: effective_spread / quoted_spread,
+            }
+        }
     };
     Ok(res)
 }
@@ -59,10 +73,14 @@ mod test {
         };
         let buy_pi = price_improvement(&quote, &Side::Buy, 95.0).unwrap();
         assert!((buy_pi.amount - 5.0).abs() < f64::EPSILON);
-        assert!((buy_pi.percentage - 0.05).abs() < f64::EPSILON);
+        assert!((buy_pi.effective_spread).abs() < f64::EPSILON);
+        assert!((buy_pi.quoted_spread - 10.0).abs() < f64::EPSILON);
+        assert!((buy_pi.efq).abs() < f64::EPSILON);
 
-        let sell_pi = price_improvement(&quote, &Side::Sell, 99.0).unwrap();
-        assert!((sell_pi.amount - 9.0).abs() < f64::EPSILON);
-        assert!((sell_pi.percentage - 0.1).abs() < f64::EPSILON);
+        let sell_pi = price_improvement(&quote, &Side::Sell, 94.0).unwrap();
+        assert!((sell_pi.amount - 4.0).abs() < f64::EPSILON);
+        assert!((sell_pi.effective_spread - 2.0).abs() < f64::EPSILON);
+        assert!((sell_pi.quoted_spread - 10.0).abs() < f64::EPSILON);
+        assert!((sell_pi.efq - 0.2).abs() < f64::EPSILON);
     }
 }
