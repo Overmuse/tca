815ecceeef6c571c2a5bf9d93ee3db3675c495ee
Sebastian Rollen <seb@overmu.se>
2021-06-17 23:03:43 -0400

    better logging

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 use anyhow::Result;
 use tca::{run, Settings};
 use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, Registry};
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let _ = dotenv::dotenv();
-    let formatting_layer = BunyanFormattingLayer::new("tca".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
+    let subscriber = tracing_subscriber::fmt()
+        .json()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
     set_global_default(subscriber).expect("Failed to set subscriber");
-    LogTracer::init().expect("Failed to set logger");
     let settings = Settings::new()?;
     run(settings).await
 }
