38fa356106c615e6c5f7128d5da6c6f6845915f2
Sebastian Rollen <seb@overmu.se>
2021-08-25 11:22:48 -0400

    upgrade alpaca version

--- src/metrics/price_improvement.rs
+++ src/metrics/price_improvement.rs
@@ -1,17 +1,22 @@
 use alpaca::Side;
 use anyhow::{anyhow, Result};
 use polygon::ws::Quote;
+use rust_decimal::prelude::*;
 use serde::Serialize;
 
 #[derive(Serialize, Debug)]
 pub struct PriceImprovement {
-    pub amount: f64,
-    pub effective_spread: f64,
-    pub quoted_spread: f64,
-    pub efq: f64,
+    pub amount: Decimal,
+    pub effective_spread: Decimal,
+    pub quoted_spread: Decimal,
+    pub efq: Decimal,
 }
 
-pub fn price_improvement(quote: &Quote, side: &Side, fill_price: f64) -> Result<PriceImprovement> {
+pub fn price_improvement(
+    quote: &Quote,
+    side: &Side,
+    fill_price: Decimal,
+) -> Result<PriceImprovement> {
     let bid_price = quote
         .bid_quote
         .as_ref()
@@ -22,11 +27,11 @@ pub fn price_improvement(quote: &Quote, side: &Side, fill_price: f64) -> Result<
         .as_ref()
         .ok_or_else(|| anyhow!("Missing ask quote"))?
         .price;
-    let mid_price = (bid_price + ask_price) / 2.0;
+    let mid_price = (bid_price + ask_price) / Decimal::TWO;
     let quoted_spread = ask_price - bid_price;
     let res = match side {
         Side::Buy => {
-            let effective_spread = 2.0 * (fill_price - mid_price);
+            let effective_spread = Decimal::TWO * (fill_price - mid_price);
             PriceImprovement {
                 amount: ask_price - fill_price,
                 effective_spread,
@@ -35,7 +40,7 @@ pub fn price_improvement(quote: &Quote, side: &Side, fill_price: f64) -> Result<
             }
         }
         Side::Sell => {
-            let effective_spread = 2.0 * (mid_price - fill_price);
+            let effective_spread = Decimal::TWO * (mid_price - fill_price);
             PriceImprovement {
                 amount: fill_price - bid_price,
                 effective_spread,
@@ -50,18 +55,19 @@ pub fn price_improvement(quote: &Quote, side: &Side, fill_price: f64) -> Result<
 #[cfg(test)]
 mod test {
     use super::*;
+    use chrono::prelude::*;
     use polygon::ws::{AskQuote, BidQuote};
 
     #[test]
     fn test() {
         let bid_quote = BidQuote {
             exchange_id: 0,
-            price: 90.0,
+            price: Decimal::new(90, 0),
             size: 100,
         };
         let ask_quote = AskQuote {
             exchange_id: 0,
-            price: 100.0,
+            price: Decimal::new(100, 0),
             size: 100,
         };
         let quote = Quote {
@@ -69,18 +75,18 @@ mod test {
             bid_quote: Some(bid_quote),
             ask_quote: Some(ask_quote),
             condition: None,
-            timestamp: 0,
+            timestamp: Utc::now(),
         };
-        let buy_pi = price_improvement(&quote, &Side::Buy, 95.0).unwrap();
-        assert!((buy_pi.amount - 5.0).abs() < f64::EPSILON);
-        assert!((buy_pi.effective_spread).abs() < f64::EPSILON);
-        assert!((buy_pi.quoted_spread - 10.0).abs() < f64::EPSILON);
-        assert!((buy_pi.efq).abs() < f64::EPSILON);
+        let buy_pi = price_improvement(&quote, &Side::Buy, Decimal::new(95, 0)).unwrap();
+        assert_eq!(buy_pi.amount, Decimal::new(5, 0));
+        assert_eq!(buy_pi.effective_spread, Decimal::ZERO);
+        assert_eq!(buy_pi.quoted_spread, Decimal::new(10, 0));
+        assert_eq!(buy_pi.efq, Decimal::ZERO);
 
-        let sell_pi = price_improvement(&quote, &Side::Sell, 94.0).unwrap();
-        assert!((sell_pi.amount - 4.0).abs() < f64::EPSILON);
-        assert!((sell_pi.effective_spread - 2.0).abs() < f64::EPSILON);
-        assert!((sell_pi.quoted_spread - 10.0).abs() < f64::EPSILON);
-        assert!((sell_pi.efq - 0.2).abs() < f64::EPSILON);
+        let sell_pi = price_improvement(&quote, &Side::Sell, Decimal::new(94, 0)).unwrap();
+        assert_eq!(sell_pi.amount, Decimal::new(4, 0));
+        assert_eq!(sell_pi.effective_spread, Decimal::new(2, 0));
+        assert_eq!(sell_pi.quoted_spread, Decimal::new(10, 0));
+        assert_eq!(sell_pi.efq, Decimal::new(2, 1));
     }
 }
